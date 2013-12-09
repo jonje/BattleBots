489f3772cb9880cbde4699b89615a9c687850743
Jonathan Jensen <haiyun211@gmail.com>
2013-12-09 10:54:21 -0700

    Line testing code

--- src/edu/neumont/lab/battlebot/BattleBot.java
+++ src/edu/neumont/lab/battlebot/BattleBot.java
@@ -1,9 +1,15 @@
 package edu.neumont.lab.battlebot;
 
+import lejos.nxt.Button;
+
 public class BattleBot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LineDetector lineDetector = new LineDetector();
+		while(!Button.ENTER.isDown()) {
+			System.out.println(lineDetector.isLineDetected());
+			
+		}
 
 	}
 
