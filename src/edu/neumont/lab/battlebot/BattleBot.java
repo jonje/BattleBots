493d89d5e8b25173892a603b18026ac059d0cbdf
Jake Wedemeyer <selslurjake1993@gmail.com>
2013-12-10 15:31:58 -0700

    battebot fix

--- src/edu/neumont/lab/battlebot/BattleBot.java
+++ src/edu/neumont/lab/battlebot/BattleBot.java
@@ -1,47 +1,44 @@
 package edu.neumont.lab.battlebot;
 
 import lejos.nxt.Button;
+import lejos.util.TextMenu;
 
 public class BattleBot {
+	private static Battler bot;
+	private static String[] options = {"Battle!","Test Drive","Test Line","Test Sensors","QUIT"};
 
 	public static void main(String[] args) throws InterruptedException {
-		Thread.sleep(1000);
-		if(!Button.ESCAPE.isDown())
+		Thread.sleep(3000);
+		boolean done = false;
+		
+		if(!Button.ENTER.isDown())
 		{
-			DriveSystem drive = new DriveSystem();
-			drive.HidePerson();
-			Thread.sleep(250);
-			
-			drive.setMovingForward();
-			Thread.sleep(1000);
-			drive.stop();
-			Thread.sleep(250);
-			
-			drive.setMovingBackward();
-			Thread.sleep(1000);
-			drive.stop();
-			Thread.sleep(250);
-			
-			drive.setTurningRight();
-			Thread.sleep(1000);
-			drive.stop();
-			Thread.sleep(250);
-			
-			drive.setTurningLeft();
-			Thread.sleep(1000);
-			drive.stop();
-			Thread.sleep(250);
-			
-			drive.setEscapeRight();
-			Thread.sleep(1000);
-			drive.stop();
-			Thread.sleep(250);
-			
-			drive.setEscapeLeft();
-			Thread.sleep(1000);
-			drive.stop();
-			Thread.sleep(250);
+			bot = new Battler();
+			TextMenu menu = new TextMenu(options);
+			while(!done)
+			{
+				switch(menu.select())
+				{
+				case(0):
+					bot.runBattle();
+				break;
+				case(1):
+					bot.testDriveSystem();
+				break;
+				case(2):
+					bot.testLineSystem();
+				break;
+				case(3):
+					bot.testSensors();
+				break;
+				case(4):
+					System.out.println("Goodbye!");
+					done = true;
+				break;
+				default:
+					System.out.println("ERROR: INVALID SELECTION");
+				}
+			}
 		}
 	}
-
 }
