5bbb9c20fe59879a29dba2bf0a81f016b9151b47
Jake Wedemeyer <selslurjake1993@gmail.com>
2013-12-11 09:52:12 -0700

    MovementLogic first implementation

--- src/edu/neumont/lab/battlebot/Battler.java
+++ src/edu/neumont/lab/battlebot/Battler.java
@@ -1,18 +1,106 @@
 package edu.neumont.lab.battlebot;
 
+import lejos.nxt.Button;
+
 public class Battler {
 	DriveSystem drive;
 	LineDetector line;
+	RobotDetector sensor;
 	
 	public Battler()
 	{
 		drive = new DriveSystem();
 		line = new LineDetector();
+		sensor = new RobotDetector();
+	}
+	
+	public boolean lineOperations()
+	{
+		boolean lineFound = false;
+		
+		if(line.isLineDetected())
+		{
+			lineFound = true;
+			drive.stop();
+			drive.setMovingBackward();
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			drive.stop();
+		}
+		
+		return lineFound;
+	}
+	
+	public void escapeOperations()
+	{
+		if(sensor.isLeftButtonPushed())
+		{
+			drive.setEscapeLeft();
+		}
+		else
+		{
+			drive.setEscapeRight();
+		}
+	}
+	
+	public boolean killOperations()
+	{
+		boolean isKilling = false;
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		isKilling = sensor.isRobotInPath();
+		
+		if(isKilling)
+		{
+			drive.setMovingForward();
+			drive.goHighSpeed();
+		}
+		
+		return isKilling;
 	}
 	
 	public void runBattle()
 	{
+		drive.HidePerson();
+		boolean waitingForLine = false;
+		boolean turn = true;
 		
+		while(!Button.ENTER.isDown())
+		{
+			if(waitingForLine)
+			{
+				waitingForLine = (lineOperations())?false:true;
+			}
+			else if(sensor.isLeftButtonPushed() || sensor.isRightButtonPushed())
+			{
+				waitingForLine = true;
+				escapeOperations();
+			}
+			else if(sensor.isRobotInPath())
+			{
+				drive.stop();
+				turn = turn?false:true;
+				waitingForLine = killOperations();
+			}
+			else
+			{
+				drive.goLowSpeed();
+				if(turn)
+					drive.setTurningRight();
+				else
+					drive.setTurningLeft();
+			}
+		}
 	}
 	
 	public void testLineSystem()
@@ -27,8 +115,6 @@ public class Battler {
 	
 	public void testDriveSystem() throws InterruptedException
 	{
-		System.out.println("DRIVE TESTS RAN");
-		/*
 		drive.stop();
 		
 		drive.HidePerson();
@@ -63,6 +149,6 @@ public class Battler {
 		Thread.sleep(1000);
 		drive.stop();
 		Thread.sleep(250);
-		*/
+		
 	}
 }
